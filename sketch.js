4330a9ae10c3370c6ed69eb96ec041d0eb8bc0e2
d9sde <156252083+d9sde@users.noreply.github.com>
2024-01-21 14:30:02 +0100

    erster Upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+// V0.2
+
+let BREITE = 400;
+let HOEHE = 400;
+let GROESSE = 15;
+
+function setup() {
+  BREITE = windowWidth;
+  HOEHE = windowHeight;
+  createCanvas(BREITE, HOEHE);
+  
+  strokeWeight(2);
+  stroke(200);
+}
+
+function draw() {
+  background(220);
+  for(a = 1; a < BREITE; a+=GROESSE)
+    {
+      for(b = 1; b < HOEHE; b+=GROESSE)
+        {
+          if(random([0,1]))
+            line(a, b, GROESSE+a, GROESSE+b);
+          else
+            line(GROESSE+a, b, a, GROESSE+b);
+        }
+    }
+  noLoop();
+}
+
+function windowResized() {
+
+  BREITE = windowWidth;
+  HOEHE = windowHeight; 
+
+  background(220);
+  resizeCanvas(windowWidth, windowHeight);
+  loop();
+}
+
+function mouseClicked() {
+  loop();
+}
+
+function keyPressed() {
+	console.log("got: key " + key + " keyCode " + keyCode);
+	if ( key === 'f' ) {  // f === 70
+		let fs = fullscreen();
+		fullscreen(!fs);
+	}
+}
